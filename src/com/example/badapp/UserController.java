acbf31666fcdb3156131dc7f5d9e7ac7702d4381
arpbhard <36184271+arpbhard@users.noreply.github.com>
2025-09-12 00:37:07 +0100

    Apply fixes to src/com/example/badapp/UserController.java

--- src/com/example/badapp/UserController.java
+++ src/com/example/badapp/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
         try {
             Statement s = DbUtil.getInstance().connection.createStatement();
             s.executeUpdate("INSERT INTO users(name,email) VALUES('" + name + "','" + email + "')");
-        } catch (Exception ex){}
+PreparedStatement stmt = DbUtil.getInstance().connection.prepareStatement(query);
     }
 
     public boolean validate(String email){
