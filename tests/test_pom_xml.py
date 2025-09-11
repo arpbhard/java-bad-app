ebb7173df077e5dc27a51945b36c4dadd5e31414
arpbhard <36184271+arpbhard@users.noreply.github.com>
2025-09-11 20:05:44 +0100

    Add unit tests for Test if the pom.xml file contains the <project> tag

--- tests/test_pom_xml.py
+++ tests/test_pom_xml.py
@@ -1,3 +1,2 @@
-def test_missing_dependency_version():
-    with pytest.raises(ValueError):
-        parse_pom_xml('<project><modelVersion>4.0.0</modelVersion><groupId>com.example</groupId><artifactId>badapp</artifactId><dependencies><dependency><groupId>commons-io</groupId><artifactId>commons-io</artifactId></dependency></dependencies></project>')
+def test_pom_xml_contains_project_tag():
+    assert '<project>' in pom_xml
