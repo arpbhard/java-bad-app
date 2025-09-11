c4f4e1305819649d299bc2842773b06a86b678a4
arpbhard <36184271+arpbhard@users.noreply.github.com>
2025-09-11 20:05:47 +0100

    Add unit tests for Test rawQuery method with valid SQL query

--- src/test/java/com/example/badapp/DbUtilTest.java
+++ src/test/java/com/example/badapp/DbUtilTest.java
@@ -1,5 +1,6 @@
 @Test
-public void testGetInstanceNotNull() {
+public void testRawQueryValidSQL() {
     DbUtil dbUtil = DbUtil.getInstance();
-    assertNotNull(dbUtil);
+    ResultSet resultSet = dbUtil.rawQuery("SELECT * FROM table");
+    assertNotNull(resultSet);
 }
