cd78499ac67c8fc65cf533f853ce86df65c14228
arpbhard <36184271+arpbhard@users.noreply.github.com>
2025-09-11 19:52:25 +0100

    Add unit tests for Test md5 method with empty input

--- src/test/java/com/example/badapp/UtilsTest.java
+++ src/test/java/com/example/badapp/UtilsTest.java
@@ -1,3 +1,5 @@
-public void testMd5NullInput() {
-    assertNull(Utils.md5(null));
+public void testMd5EmptyInput() {
+    String input = "";
+    String expectedResult = "d41d8cd98f00b204e9800998ecf8427e";
+    assertEquals(expectedResult, Utils.md5(input));
 }
