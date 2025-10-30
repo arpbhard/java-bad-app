d5cb02a8b99a12e595b1a40d483a3afaf5b21e1b
Arpit Bhardwaj <36184271+arpbhard@users.noreply.github.com>
2025-10-30 13:34:09 +0000

    Add unit tests for Test the main method with a null command line argument

--- src/test/java/com/example/badapp/MainTest.java
+++ src/test/java/com/example/badapp/MainTest.java
@@ -1,10 +1,10 @@
 public class MainTest {
 
     @Test
-    public void testMainMethodWithoutArgs() {
+    public void testMainMethodWithNullArg() {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        Main.main(new String[0]);
+        Main.main(new String[]{null});
         assertEquals("Starting bad Java app...\n", outContent.toString());
         System.setOut(System.out);
     }
