989630fade246a911468bfc9efdc94d39e64092f
Arpit Bhardwaj <36184271+arpbhard@users.noreply.github.com>
2025-11-10 10:55:35 +0000

    Add unit tests for Test the main method with no arguments provided

--- src/test/java/com/example/badapp/MainTest.java
+++ src/test/java/com/example/badapp/MainTest.java
@@ -1,3 +1,7 @@
-public void testGlobalStateValue() {
-    assertEquals("mutable", Main.GLOBAL_STATE);
+@Test
+public void testMainWithNoArgs() {
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    Main.main(new String[]{});
+    assertEquals("Starting bad Java app...\n", outContent.toString());
 }
