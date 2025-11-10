e3d2520a807571289881b48bc7b1520db91baca4
Arpit Bhardwaj <36184271+arpbhard@users.noreply.github.com>
2025-11-10 10:54:10 +0000

    Add unit tests for Test if main method assigns user argument correctly

--- src/test/java/com/example/badapp/MainTest.java
+++ src/test/java/com/example/badapp/MainTest.java
@@ -1,8 +1,9 @@
-public void testMainMethodWithNullArgs() {
+public void testMainMethodWithUserArg() {
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
 
-    Main.main(null);
+    Main.main(new String[] {"John"});
 
     assertEquals("Starting bad Java app...\n", outContent.toString());
+    assertEquals("John", Main.user);
 }
