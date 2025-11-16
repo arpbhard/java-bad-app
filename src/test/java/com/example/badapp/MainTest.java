f736e43c3cd59acac644f287337bca2d83b5e47f
Arpit Bhardwaj <36184271+arpbhard@users.noreply.github.com>
2025-11-16 11:38:31 +0000

    Add unit tests for Test if main method works correctly when a user argument is provided

--- src/test/java/com/example/badapp/MainTest.java
+++ src/test/java/com/example/badapp/MainTest.java
@@ -1,7 +1,7 @@
 @Test
-public void testMainMethodWithNoArguments() {
+public void testMainMethodWithUserArgument() {
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
-    Main.main(new String[]{});
+    Main.main(new String[]{"John"});
     assertEquals("Starting bad Java app...\n", outContent.toString());
 }
