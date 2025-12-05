6db26a6afd60629173d200583c7996322b38c4af
Arpit Bhardwaj <36184271+arpbhard@users.noreply.github.com>
2025-12-05 20:32:25 +0530

    Add unit tests for Test main method without arguments provided

--- src/test/java/com/example/badapp/MainTest.java
+++ src/test/java/com/example/badapp/MainTest.java
@@ -1,7 +1,7 @@
-public void testMainWithArguments() {
+public void testMainWithoutArguments() {
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
-    String[] args = {"John"};
+    String[] args = {};
     Main.main(args);
     assertEquals("Starting bad Java app...\n", outContent.toString());
 }
